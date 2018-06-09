d43112cc138fe66c8669e37e304dc17bc5c98585
Weirdfish31 <weirdfish31@whu.edu.cn>
2018-06-10 01:21:55 +0800

    instantiation of the readhelper

---
+++ WDNwordPro/configHandle.py
@@ -0,0 +1,27 @@
+import time
+import readhelper
+# starttime = datetime.datetime.strptime('3 Nov 2013 04:00:00', '%d %b %Y %H:%M:%S')
+
+def handlefun():
+    s = time.clock()
+    readconfig = readhelper.readhelper()
+#    print(readconfig.gnodelist)
+#    print(readconfig.nodelist)
+    readconfig.createlinktop()
+    readconfig.csvreadhandle()
+    readconfig.generateGStopFault()
+    readconfig.generateGGapp()
+    
+# =============================================================================
+#     readconfig.generateTCPwindow()
+#     readconfig.generateTheFaultFile()
+# =============================================================================
+    e = time.clock()
+    print('time is ' + str(e - s) + 's')
+    for x in readconfig.GtoSatList:
+        print(x)
+
+if __name__ == '__main__':
+     handlefun()
+
+
