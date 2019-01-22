c17c26c7f67ea2d49b4568335b09b7752a6f1405
Weirdfish31 <weirdfish31@whu.edu.cn>
2019-01-22 12:53:15 +0800

    Add files via upload

--- WDNwordPro/WDNTagDataHandler.py
+++ WDNwordPro/WDNTagDataHandler.py
@@ -264,6 +264,123 @@ class TaggedDataHandler:
         """
         初始化
         """
+    def GridDataTagWriter(self,vbrs=24000,count_i=20,superappsize=[16000,30000],trafsize=[8000,10000,12000,14000,16000,18000,20000,22000,24000,26000,28000,30000,32000,34000,36000],datapath='D:/WDNoptimizer/2DGMM(16000_8000-36000)/',savefilename='./LabelData/2DGMM(16000_8000-36000).txt'):
+        """
+        读取主观栅格数据
+        """
+        superappinterval=[20]
+        vbrinterval=[30]
+        vbrsize=[]
+        vbrsize.append(vbrs)
+        trafinterval=[30]
+        memoryset=WDNoptimizer.MemoryUnit()#记忆单元，存储每次的状态
+        iternum=0
+        "读取训练数据集================================================================"
+        """用来读取原始数据集，得到memoryset.probmemoryunit，绘制聚类图，拟合的GMM热力图"""
+        for sappi_i in superappinterval:
+            for sapps_i in superappsize:
+                for vbri_i in vbrinterval:
+                    for vbrs_i in vbrsize:
+                        for trafi_i in trafinterval: 
+                            for trafs_i in trafsize:
+                                """
+                                gamer:对每一次一个输入组合的全部采样点做一次聚类
+                                """
+                                gamer=WDNoptimizer.GMMvalueOptimizaitonUnit(cluster=2)#用来对一组参数中的数据聚类
+                                tempmemoryset=WDNoptimizer.MemoryUnit()
+                                state=[sappi_i,sapps_i,vbri_i,vbrs_i,trafi_i,trafs_i]
+                                for i in range(count_i):
+                                    """
+                                    读取数据，对数据进行分类处理
+                                    """
+                                    dataset='radio REQUEST-SIZE DET '+str(sapps_i)+' _ '+str(vbrs_i)+' _ RND DET '+str(trafs_i)+' _'+str(i)
+                                    readdb=WDNexataReader.ExataDBreader()#实例化
+                                    readdb.opendataset(dataset,datapath)#读取特定路径下的数据库
+                                    readdb.appnamereader()#读取业务层的业务名称
+                                    readdb.appfilter()#将业务名称分类至三个list
+                                    readdb.appdatareader()#将每个业物流的输出数据存到实例化的类中的字典里面
+                                    readdb.inputparainsert(sappi_i,sapps_i,vbri_i,vbrs_i,trafi_i,trafs_i)
+                                    "======================以上步骤不可省略，下方处理可以根据需求修改"
+                                    """
+                                    评估部分，对于三种不同的业务有不同的权重:时延、抖动、丢包率、吞吐量
+                                    vbr:        [1,2,3,4] delay,jitter,messagecompleterate,throughtput
+                                    trafficgen: [5,6,7,8]
+                                    superapp:   [9,10,11,12]
+                                    vbr,superapp,trafficgen
+                                    """
+                                    eva=WDNoptimizer.EvaluationUnit()
+                                    vbr=readdb.meandata('vbr')
+                                    eva.calculateMetricEvaValue(vbr)
+                                    trafficgen=readdb.meandata('trafficgen')
+                                    eva.calculateMetricEvaValue(trafficgen)
+                                    superapp=readdb.meandata('superapp')
+                                    eva.calculateMetricEvaValue(superapp)
+                                    value=eva.evaluationvalue()
+                                    """
+                                    状态动作保存：当前状态、评估值、动作、收益(目前的动作和收益没有用暂时放这里)
+                                    如果是第一次仿真，动作与收益为缺省值null
+                                    记忆单元的数据包括：
+                                        1）当前状态：6个输入量 三个业务的包大小，发包时间间隔
+                                        2）评估值：value=eva.evaluationvalue()
+                                    """
+                                    print(state)
+                                    """
+                                    "memoryset用来保存原始数据信息" 
+                                    "tempmemoryset用来进行读取数据是的聚类"
+                                    """
+                                    memoryset.valueinserter(state=state,value=value)
+                                    tempmemoryset.valueinserter(state=state,value=value)
+                                    print(eva.normalizedata)
+                                """
+                                "去掉nan数据"
+                                "聚类"
+                                "聚类后的数据保存到memoryset.probmemoryunit中"，最终得到每簇的均值、标签、概率
+                                """
+                                tempdataset=gamer.dropNaNworker(tempmemoryset.memoryunit)
+                                tempdataset=gamer.presortworker(tempdataset,col1='vbri',col2='value')
+                                tempdataset=gamer.clusterworker(tempdataset,col1='vbri',col2='value',count=iternum)
+                                a=np.mean(tempdataset[tempdataset['label']==0]['value'])
+                                b=np.mean(tempdataset[tempdataset['label']==1]['value'])   
+                                if a<b:
+                                    part0=tempdataset.loc[tempdataset['label']==0]
+                                    part0.loc[:,'label']=0
+                                    part1=tempdataset.loc[tempdataset['label']==1]
+                                    part1.loc[:,'label']=1
+                                    """
+                                    计算混合模型中第一簇的概率，目前问题中的模型分为两簇，
+                                    计算一簇模型的概率自然可以得到另一簇的概率
+                                    """
+                                    probOf1=len(part1)/len(tempdataset)
+                                    probOf0=1-probOf1
+                                    value1=np.mean(part1[part1['label']==1]['value'])
+                                    value0=np.mean(part0[part0['label']==0]['value'])
+                                    memoryset.probinserter(state=state,value=value1,prob=probOf1,label=1)
+                                    memoryset.probinserter(state=state,value=value0,prob=probOf0,label=0)
+                                elif a>b:
+                                    part0=tempdataset.loc[tempdataset['label']==0]
+                                    part0.loc[:,'label']=1
+                                    part1=tempdataset.loc[tempdataset['label']==1]
+                                    part1.loc[:,'label']=0
+                                    probOf1=len(part0)/len(tempdataset)
+                                    probOf0=1-probOf1
+                                    value1=np.mean(part0[part0['label']==1]['value'])
+                                    value0=np.mean(part1[part1['label']==0]['value'])
+                                    memoryset.probinserter(state=state,value=value1,prob=probOf1,label=1)
+                                    memoryset.probinserter(state=state,value=value0,prob=probOf0,label=0)                            
+                                iternum=iternum+1
+                                
+        "数据预处理====目前预处理都是在读取数据时完成===================================="
+#        priordataset=memoryset.memoryunit#将原始的数据保存到内存中
+        print(memoryset.probmemoryunit)#这个数据是value均值、分簇概率，标签的综合数据，下面将利用这个数据进行GMM建模
+#        print(priordataset)#原始数据包括state，value
+        with open(savefilename,'w') as f:#记录每次AF选点的参数
+            f.write('\n')
+            f.write(str(memoryset.probmemoryunit))#写入标记过的数据的DataFrame  
+        print("mission completed！！！")
+               
+        
+        
+        
     def IterDataTagWriter(self,vbrs=24000,count_i=10,path='E:/WDNoptimizer/GMM_i60_t10/',QPlist=[],savefilename='./LabelData/ITER_GMM24000_i60_t10.txt'):
         """
         读取QP列表的点，对迭代数据进行处理
